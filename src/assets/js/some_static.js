8757a3624665747051691dd717e49d0d6ee61703
Oscar D. Corbalan <oscardc@gmx.com>
2016-11-03 20:31:14 +0100

    feat: Improve generated .html
    Closes #9

--- src/assets/js/some_static.js
+++ src/assets/js/some_static.js
@@ -1,9 +1,9 @@
-/* 
+/*
  * Drop your static JS libraries in this folder.
  * For example, JQuery JS would go in here if
  * you aren't loading it from their CDN.
  */
-
-console.log('Hello, I am a static javascript file copied from ./src/assets/js to ./public/js');
-console.log('I am available at http://gulp.dev/js/some_static.js');
-console.log('Just load me with: <script src="/js/some_static.js"></script>');
+ 
+function assetsFunction(){
+	alert('Hello, I am a static javascript file copied from ./src/assets/js to ./public/js');
+}
